4daefde9e56c038bbfdf33b93a4643ecd9c63ad2
LevantFlowAPP <alaamohammad20012@gmail.com>
2025-02-09 06:26:06 +0300

    generl

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-const withMDX = require('@next/mdx')()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     domains: ['i3.ytimg.com'],
     unoptimized: true,
-  },
-  experimental: {
-    mdxRs: true,
-  },
+  }
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
