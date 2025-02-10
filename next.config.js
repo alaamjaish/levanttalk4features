0267a9ad0b7b2a99eca7bdf5561a335eaae330f5
LevantFlowAPP <alaamohammad20012@gmail.com>
2025-02-10 04:38:21 +0300

    Update .gitignore and next.config.js for MDX support and Docker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     domains: ['i3.ytimg.com'],
     unoptimized: true,
-  }
+  },
+  experimental: {
+    mdxRs: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
